5a6cc02a1b34d83d2fd582f4afcdbc31ac718e91
itto4869 <itto4869@gmail.com>
2025-08-29 19:07:21 +0900

    abc046: d is solved.

--- abc046/src/bin/d.rs
+++ abc046/src/bin/d.rs
@@ -2,9 +2,39 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [i64; n],
+        s: String,
     }
-    let ans: i64 = a.iter().sum();
-    println!("{ans}");
+    let s_bytes = s.as_bytes();
+    let mut p_count = 0;
+    let mut g_count = 0;
+    let mut result = Vec::with_capacity(s.len());
+    for &b in s_bytes {
+        if p_count == g_count {
+            g_count += 1;
+            match b {
+                b'p' => {
+                    result.push(-1);
+                }
+                b'g' => {
+                    result.push(0);
+                }
+                _ => unreachable!(),
+            }
+        }
+        else {
+            match b {
+                b'p' => {
+                    p_count += 1;
+                    result.push(0);
+                }
+                b'g' => {
+                    p_count += 1;
+                    result.push(1);
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    let ans = result.iter().sum::<i32>();
+    println!("{}", ans);
 }
