14b447ea018227a0e7b173f0c5e0a9edaab277bb
itto4869 <itto4869@gmail.com>
2025-09-04 23:09:10 +0900

    abc047: c is solved.

--- abc047/src/bin/c.rs
+++ abc047/src/bin/c.rs
@@ -2,9 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [i64; n],
+        s: String,
     }
-    let ans: i64 = a.iter().sum();
-    println!("{ans}");
+    let s = s.chars().collect::<Vec<char>>();
+    let mut ans = 0;
+    for i in 1..s.len() {
+        if s[i] != s[i - 1] {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
