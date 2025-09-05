117fbaf6b408520658b18e28be9f49901047646a
itto4869 <itto4869@gmail.com>
2025-09-05 15:38:08 +0900

    abc047: d is solved.

--- abc047/src/bin/d.rs
+++ abc047/src/bin/d.rs
@@ -3,8 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        t: u64,
         a: [i64; n],
     }
-    let ans: i64 = a.iter().sum();
-    println!("{ans}");
+    let mut max_difference = 0;
+    let mut min_price = std::i64::MAX;
+    let mut count = 0;
+    for &price in &a {
+        min_price = min_price.min(price);
+        let difference = price - min_price;
+        if difference == max_difference {
+            count += 1;
+        } else if difference > max_difference {
+            max_difference = difference;
+            count = 1;
+        } else {
+        }
+    }
+    println!("{}", count);
 }
