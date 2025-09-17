1432e187b87edb0102c9b4d660598a2bf5103aca
itto4869 <itto4869@gmail.com>
2025-09-17 19:50:08 +0900

    abc051: d is not solved

--- abc051/src/bin/d.rs
+++ abc051/src/bin/d.rs
@@ -1,10 +1,35 @@
 use proconio::input;
 
+const INF: i64 = 1 << 29;
 fn main() {
     input! {
         n: usize,
-        a: [i64; n],
+        m: usize,
+        edge: [(usize, usize, i64); m], 
     }
-    let ans: i64 = a.iter().sum();
-    println!("{ans}");
+    let mut dist = vec![vec![INF; n]; n];
+    for i in 0..n {
+        dist[i][i] = 0;
+    }
+
+    for (a, b, c) in &edge {
+        dist[a - 1][b - 1] = *c;
+        dist[b - 1][a - 1] = *c;
+    }
+
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                dist[i][j] = dist[i][j].min(dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for (a, b, c) in edge {
+        if c > dist[a - 1][b - 1] {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
