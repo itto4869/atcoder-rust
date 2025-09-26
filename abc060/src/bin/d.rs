facdb814d6d4994eeaf16f878df7dd6404252049
itto4869 <itto4869@gmail.com>
2025-09-26 17:37:49 +0900

    abc060: little change

--- abc060/src/bin/d.rs
+++ abc060/src/bin/d.rs
@@ -6,14 +6,11 @@ fn main() {
         w: u64,
         wv: [(u64, u64); n],
     }
-    let mut dp = vec![0u64; (w + 1) as usize];
-    for i in 0..n {
-        let (weight, value) = wv[i];
-        for j in (weight..=w).rev() {
-            dp[j as usize] = dp[j as usize].max(dp[(j - weight) as usize] + value as u64);
-        }
+    let mut items = vec![Vec::new(); 4];
+    for (wi, vi) in &wv {
+        items[(wi - wv[0].0) as usize].push(*vi);
     }
-    let ans = dp[w as usize];
-    println!("{}", ans);
+        
 }
+
 
