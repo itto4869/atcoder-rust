b5ea86b4dbf036f1ebbf8d4e412451f0a99c5744
itto4869 <itto4869@gmail.com>
2025-10-01 21:24:26 +0900

    abc063: a, b and c are solved

--- abc063/src/bin/c.rs
+++ abc063/src/bin/c.rs
@@ -4,4 +4,26 @@ fn main() {
     input! {
         n: usize,
     };
+    let mut sum = 0;
+    let mut points = Vec::new();
+    for _ in 0..n {
+        input! {
+            s: u64,
+        };
+        sum += s;
+        points.push(s);
+    }
+    if sum % 10 != 0 {
+        println!("{}", sum);
+        return;
+    } else {
+        points.sort();
+        for p in points {
+            if p % 10 != 0 {
+                println!("{}", sum - p);
+                return;
+            }
+        }
+        println!("0");
+    }
 }
--- abc063/src/bin/d.rs
+++ abc063/src/bin/d.rs
@@ -1,5 +1,31 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        a: i64,
+        b: i64,
+        mut h: [i64; n],
+    };
+    h.sort();
+    h.reverse();
+    let mut ok = h[0] / b;
+    let mut ng = 0;
+    while ok - ng > 1 {
+        let mid = (ok + ng) / 2;
+        let mut need = 0;
+        for &hi in &h {
+            let rem = hi - mid * b;
+            if rem > 0 {
+                need += (rem + a - b - 1) / (a - b);
+            }
+        }
+        if need <= mid {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    println!("{}", ok);
 }
